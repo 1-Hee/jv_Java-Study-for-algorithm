4fd9ed1299b1c7f002f66f7142abbf54c609b127
1-Hee <onehee9710@gmail.com>
2022-04-11 20:56:56 +0900

    HashTable

---
+++ HashTableSearch.java
@@ -0,0 +1,59 @@
+package algobook_src;
+
+import java.util.Scanner;
+
+public class HashTableSearch {
+	
+	// 해시 테이블의 실체가 되는 배열(요소 수를 10개로 함)
+	public static int[] hashTable = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+	
+	// 해시 함수 역할을 하는 메소드
+	public static int hashFunc(int data) {
+		return data%10;		
+	}
+	
+	// 프로그램 실행의 시작 점인 main 메소드
+	public static void main(String[] args) {
+		int data, hashValue;
+		// 키보드로 데이터를 입력 하여 해시테이블에 저장
+		Scanner sc = new Scanner(System.in);
+		do {
+			//저장할 데이터 입력
+			System.out.printf("\n 저장할 데이터 = ");
+			data = sc.nextInt();
+			if(data<0) {
+				break;
+			}
+			
+			// 해시 값을 구함			
+			hashValue = hashFunc(data);
+			
+			// 해시 테이블에 저장
+			hashTable[hashValue] = data;						
+			
+		} while(true);
+		
+		// 해시 테이블에서 데이터를 탐색
+		do {
+			// 검색할 데이터를 키보드로 입력
+			System.out.printf("\n 탐색할 데이터 = ");
+			data = sc.nextInt();
+			
+			if(data < 0 ){
+				break;
+			}
+			
+			// 해시 값을 구함
+			hashValue = hashFunc(data);
+			
+			// 검색한 결과를 표시
+			if(hashTable[hashValue]==data) {
+				System.out.printf("%d번째에서 발견되었습니다. \n", hashValue);
+			} else {
+				System.out.printf("찾을 수가 없습니다. ");
+			}
+		} while(true);
+		sc.close();
+	}
+
+}
